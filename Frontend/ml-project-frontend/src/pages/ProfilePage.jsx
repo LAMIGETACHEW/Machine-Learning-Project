988a15b97cdf2192132a8301a09d2643759de772
LAMIGETACHEW <lamichache@gmail.com>
2025-05-18 13:50:15 +0300

    Login Page and Register Page done

---
+++ Frontend/ml-project-frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import HealthProfileForm from "../components/Profile/HealthProfileForm";
+export default function ProfilePage() {
+  return (
+    <div>
+      <HealthProfileForm />
+    </div>
+  );
+}
---
+++ Frontend/ml-project-frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import RegisterForm from "../components/Auth/RegisterForm";
+export default function RegisterPage() {
+  return (
+    <div>
+      <h2>Register</h2>
+      <RegisterForm />
+    </div>
+  );
+}
